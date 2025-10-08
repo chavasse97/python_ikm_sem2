58460858440f1989014fe59e6e8c9182b1480c2a
Egor Rogozin <99vanders@gmail.com>
2025-10-08 09:09:21 +0300

    Update the1_2.py

--- the1_2.py
+++ the1_2.py
@@ -3,8 +3,8 @@ mylist = line.split()
 summ = 0
 for item in range(len(mylist), -1, -1):
   try:
-    print(mylist[-1], end=' ')
     summ += int(mylist[-1])
+    print(mylist[-1], end=' ')
     mylist.pop()
   except ValueError:
     print('current_root =', len(mylist))
